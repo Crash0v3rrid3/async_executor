79637f82ef19828fed31f585e9bb26c8cce1a03f
Crash0v3rrid3 <jain240298@gmail.com>
2021-08-12 20:41:02 +0530

    Moved executor primary code from __init__.py to async_executor.py & implemented proxy_impl for AsyncToSyncMixin support

--- async_executor/__init__.py
+++ async_executor/__init__.py
@@ -1,90 +1,13 @@
-import asyncio
-import inspect
-import threading
-from concurrent import futures
-from functools import wraps
-from typing import Awaitable, Tuple, AsyncGenerator
-
-__event_loop = None
-
-
-def _func():
-    asyncio.set_event_loop(__event_loop)
-    __event_loop.run_forever()
-
-
-def start_worker_thread():
-    # Thread locals will not be copied
-    # Make sure to send all required args to the coroutine call
-    __thread = threading.Thread(target=_func, name="looper", daemon=True)
-    __thread.start()
-
-
-def set_event_loop(loop: asyncio.AbstractEventLoop):
-    """Call this to initialize async executor.
-    NOTE: Should be called once per application. This is to maintain a single
-    event loop throughout the app. Also, call start_worker_thread if the app
-    doesn't have a primary thread for running the async jobs
-    """
-    global __event_loop
-    if __event_loop is not None:
-        return
-    __event_loop = loop
-
-
-def get_event_loop():
-    """Call this to initialize async executor.
-    NOTE: Should be called once per application. This is to maintain a single
-    event loop throughout the app.
-    """
-    assert __event_loop is not None, "Call set_event_loop first!"
-    return __event_loop
-
-
-def run_async_job(cor: Awaitable) -> futures.Future:
-    """Use this to run a task asynchronously."""
-    assert __event_loop is not None, "Call set_event_loop first!"
-    return asyncio.run_coroutine_threadsafe(cor, loop=__event_loop)
-
-
-def complete_async_jobs(*cors: Awaitable) -> Tuple[futures.Future, ...]:
-    """Use this to optimize execution by delegating I/O tasks to async concurrency"""
-    assert __event_loop is not None, "Call set_event_loop first!"
-    return asyncio.run_coroutine_threadsafe(
-        asyncio.wait(cors, loop=__event_loop),
-        loop=__event_loop,
-    ).result()[0]
-
-
-def agen_to_gen(agen: AsyncGenerator):
-    """Async generator to generator"""
-    assert __event_loop is not None, "Call set_event_loop first!"
-    while True:
-        try:
-            yield run_async_job(agen.__anext__()).result()
-        except StopAsyncIteration:
-            break
-
-
-def async_to_sync(cor):
-    """Decorator used to convert async function to sync.
-    This would do exactly what cor would, raise exceptions if any, accept the same args etc.
-    """
-
-    if inspect.isasyncgenfunction(cor):
-
-        @wraps(cor)
-        def inner(*args, **kwargs):
-            yield from agen_to_gen(cor(*args, **kwargs))
-
-        return inner
-
-    @wraps(cor)
-    def inner(*args, **kwargs):
-        return run_async_job(cor(*args, **kwargs)).result()
-
-    return inner
-
+from .async_executor import (
+    run_async_job,
+    complete_async_jobs,
+    async_to_sync,
+    set_event_loop,
+    get_event_loop,
+    agen_to_gen,
+    start_worker_thread,
+)
+from .proxy_impl import AsyncToSyncMixin, force_async
 
 __all__ = [
     "run_async_job",
@@ -93,5 +16,7 @@ __all__ = [
     "set_event_loop",
     "get_event_loop",
     "agen_to_gen",
-    "start_worker_thread"
+    "start_worker_thread",
+    "force_async",
+    "AsyncToSyncMixin",
 ]
---
+++ async_executor/proxy_impl.py
@@ -0,0 +1,89 @@
+import asyncio
+import inspect
+from contextlib import contextmanager
+from threading import local
+
+from async_executor import async_to_sync
+
+_state = local()
+
+
+@contextmanager
+def force_async():
+    """Use this to force AsyncToSyncMixin.__getattr__ to always return a coroutine.
+    Useful for running multiple calls asynchronously in the primary thread at a time.
+    """
+    try:
+        _state.force_async = True
+        yield
+    finally:
+        _state.force_async = False
+
+
+def get_current_task():
+    """
+    Cross-version implementation of asyncio.current_task()
+
+    Returns None if there is no task.
+    """
+    try:
+        if hasattr(asyncio, "current_task"):
+            # Python 3.7 and up
+            return asyncio.current_task()
+        else:
+            # Python 3.6
+            return asyncio.Task.current_task()
+    except RuntimeError:
+        return None
+
+
+def _should_wrap(item):
+    return asyncio.iscoroutinefunction(item) or inspect.isasyncgenfunction(item)
+
+
+class AsyncToSyncMixin:
+    """A utility to patch any class with async API to work with sync code.
+    It patches the __getattribute__ method to decide whether to return a coroutine to
+    the caller or to return a function which wraps the coroutine, runs it and returns it
+    result once done. It does this based on two parameters
+    - Is the force_async context enabled or
+    - Is the current execution happening under an async task in the primary event loop.
+
+    Useful when working with sync frameworks like Django and you want take full advantage of
+    async concurrency.
+    """
+    def __enter__(self):
+        try:
+            enter = super().__enter__
+        except AttributeError as e:
+            try:
+                enter = async_to_sync(super().__aenter__)
+            except AttributeError:
+                raise e
+        return enter()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        try:
+            exit_ = super().__exit__
+        except AttributeError as e:
+            try:
+                exit_ = async_to_sync(super().__aexit__)
+            except AttributeError:
+                raise e
+        return exit_(exc_type, exc_val, exc_tb)
+
+    def __getattribute__(self, name):
+        item = super().__getattribute__(name)
+
+        if _should_wrap(item) is False:
+            return item
+
+        if (
+            getattr(_state, "force_async", False) is True
+            or get_current_task() is not None
+        ):
+            return item
+        return async_to_sync(item)
+
+
+__all__ = ["AsyncToSyncMixin", "force_async"]
